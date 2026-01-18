7a79fb2f613e3759ab5be5627d2b76c10f102531
Scherbakoff <elkirrs@gmail.com>
2026-01-19 00:08:21 +0300

    quiz

--- app/vite.config.mjs
+++ app/vite.config.mjs
@@ -4,87 +4,87 @@ import Components from 'unplugin-vue-components/vite'
 import Fonts from 'unplugin-fonts/vite'
 import Layouts from 'vite-plugin-vue-layouts-next'
 import Vue from '@vitejs/plugin-vue'
-import { VueRouterAutoImports } from 'unplugin-vue-router'
-import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import {VueRouterAutoImports} from 'unplugin-vue-router'
+import Vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import {defineConfig} from 'vite'
+import {fileURLToPath, URL} from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/quiz/",
-  plugins: [
-    Layouts(),
-    Vue({
-      template: { transformAssetUrls },
-    }),
-    // viteSingleFile(), //for generate single page
-    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
-    Vuetify({
-      autoImport: true,
-      styles: {
-        configFile: 'src/styles/settings.scss',
-      },
-    }),
-    Components(),
-    Fonts({
-      google: {
-        families: [{
-          name: 'Roboto',
-          styles: 'wght@100;300;400;500;700;900',
-        }],
-      },
-    }),
-    AutoImport({
-      imports: [
-        'vue',
-        VueRouterAutoImports,
-        {
-          'pinia': ['defineStore', 'storeToRefs'],
+    base: "/quiz/",
+    plugins: [
+        Layouts(),
+        Vue({
+            template: {transformAssetUrls},
+        }),
+        // viteSingleFile(), //for generate single page
+        // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
+        Vuetify({
+            autoImport: true,
+            styles: {
+                configFile: 'src/styles/settings.scss',
+            },
+        }),
+        Components(),
+        Fonts({
+            google: {
+                families: [{
+                    name: 'Roboto',
+                    styles: 'wght@100;300;400;500;700;900',
+                }],
+            },
+        }),
+        AutoImport({
+            imports: [
+                'vue',
+                VueRouterAutoImports,
+                {
+                    'pinia': ['defineStore', 'storeToRefs'],
+                },
+            ],
+            eslintrc: {
+                enabled: true,
+            },
+            vueTemplate: true,
+        }),
+    ],
+    optimizeDeps: {
+        exclude: [
+            'vuetify',
+            'vue-router',
+            'unplugin-vue-router/runtime',
+            'unplugin-vue-router/data-loaders',
+            'unplugin-vue-router/data-loaders/basic',
+        ],
+    },
+    define: {'process.env': {}},
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
-      ],
-      eslintrc: {
-        enabled: true,
-      },
-      vueTemplate: true,
-    }),
-  ],
-  optimizeDeps: {
-    exclude: [
-      'vuetify',
-      'vue-router',
-      'unplugin-vue-router/runtime',
-      'unplugin-vue-router/data-loaders',
-      'unplugin-vue-router/data-loaders/basic',
-    ],
-  },
-  define: { 'process.env': {} },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+        extensions: [
+            '.js',
+            '.json',
+            '.jsx',
+            '.mjs',
+            '.ts',
+            '.tsx',
+            '.vue',
+        ],
     },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
-  },
-  server: {
-    port: 5173,
-  },
-  css: {
-    preprocessorOptions: {
-      sass: {
-        api: 'modern-compiler',
-      },
-      scss: {
-        api: 'modern-compiler',
-      },
+    server: {
+        port: 5173,
     },
-  },
+    css: {
+        preprocessorOptions: {
+            sass: {
+                api: 'modern-compiler',
+            },
+            scss: {
+                api: 'modern-compiler',
+            },
+        },
+    },
 })
